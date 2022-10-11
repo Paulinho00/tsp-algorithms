2b76bf4023741a7286ed218c16c7c6fc6931f160
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2022-10-11 19:07:26 +0200

    tsp brute force filenames refactor

--- tsp_brute_force/tsp_brute_force.py
+++ tsp_brute_force/tsp_brute_force.py
@@ -18,23 +18,30 @@ def tsp_brute_force(adjacency_matrix, vertex_number):
     vertexes = list(range(1, vertex_number))
     paths = list(permutations(vertexes))
     min_path = maxsize
+    min_vertex_path = []
     for path in paths:
         current_pathweight = 0
         current_vertex = 0
+        vertex_path = [0]
         for vertex in path:
             current_pathweight += adjacency_matrix[current_vertex][vertex]
             current_vertex = vertex
+            vertex_path.append(vertex)
         current_pathweight += adjacency_matrix[current_vertex][0]
-        min_path = min(current_pathweight, min_path)
 
-    return min_path
+        if min_path > current_pathweight:
+            min_path = current_pathweight
+            min_vertex_path = vertex_path
+
+    return (min_path, min_vertex_path)
 
 
 def main():
     adjacency_matrix = []
     vertex_number = read_file("tsp_10.txt", adjacency_matrix)
     result = tsp_brute_force(adjacency_matrix, vertex_number)
-    pass
+    print(result)
+
 
 if __name__ == '__main__':
     main()
