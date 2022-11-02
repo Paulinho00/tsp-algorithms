754ae972f75293361f4a2310cdf303a45a2aa297
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2022-11-02 14:58:20 +0100

    Add bnb bfs

--- bnb/bnb.py
+++ bnb/bnb.py
@@ -16,6 +16,7 @@ class Node:
         self.c = c
         self.matrix = matrix
 
+
 def  reduce_matrix(matrix):
     c = 0
     min_rows = matrix.min(axis=1)
@@ -37,12 +38,14 @@ def  reduce_matrix(matrix):
 
     return reduced_matrix, c
 
+
 def insert_inf(matrix, vertex1, vertex2):
     new_matrix = np.copy(matrix)
     new_matrix[vertex1] = math.inf
     new_matrix[:,vertex2] = math.inf
     new_matrix[vertex1][vertex2] = math.inf
     return new_matrix
+
 
 def bnb_dfs(adjacency_matrix, graph, vertex_number):
     min_bound = math.inf
@@ -68,12 +71,34 @@ def bnb_dfs(adjacency_matrix, graph, vertex_number):
             new_matrix, c = reduce_matrix(new_matrix)
             sum_cost = node.matrix[node.vertex][neighbor] + node.c + c
             stack.append(Node(neighbor, node.path + [neighbor], sum_cost, new_matrix))
-
-
     return min_cost, path
 
 
+def bnb_bfs(adjacency_matrix, graph, vertex_number):
+    min_bound = math.inf
+    min_cost = math.inf
+    adjacency_matrix, c = reduce_matrix(adjacency_matrix)
+    path = []
+    path.append(0)
+    queue = [Node(0, path, c, adjacency_matrix)]
+    while len(queue) > 0:
+        node = queue.pop(0)
+        if node.c > min_bound:
+            continue
+        if len(node.path) == vertex_number:
+            min_bound = node.c
+            min_cost = min_bound
+            node.path.append(0)
+            path = node.path
 
+        for neighbor in graph.neighbors(node.vertex):
+            if neighbor in node.path:
+                continue
+            new_matrix = insert_inf(node.matrix, node.vertex, neighbor)
+            new_matrix, c = reduce_matrix(new_matrix)
+            sum_cost = node.matrix[node.vertex][neighbor] + node.c + c
+            queue.append(Node(neighbor, node.path + [neighbor], sum_cost, new_matrix))
+    return min_cost, path
 
 
 def main():
@@ -90,9 +115,9 @@ def main():
         writer.writerow([parameters[0], parameters[1], parameters[2], parameters[3]])
         for i in range(0, int(parameters[1])):
             start = time.perf_counter()
-            result = bnb_dfs(adjacency_matrix, graph, vertex_number)
+            result = bnb_bfs(adjacency_matrix, graph, vertex_number)
             end = time.perf_counter()
-            mem_usage = memory_usage((bnb_dfs, (adjacency_matrix, graph, vertex_number)))
+            mem_usage = memory_usage((bnb_bfs, (adjacency_matrix, graph, vertex_number)))
             writer.writerow([format_scientific(end - start, locale="pl_Pl"), result[0], result[1], max(mem_usage)])
             print([format_scientific(end - start, locale="pl_Pl"), result[0], result[1]], max(mem_usage))
 
