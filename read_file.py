53232f8000b11260ff785daa448e767e24da9709
Paweł Gryglewicz <pawel.gryglewicz09@gmail.com>
2022-11-02 14:22:00 +0100

    Matrix include inf after read

--- read_file.py
+++ read_file.py
@@ -1,3 +1,5 @@
+import math
+
 import networkx
 import numpy as np
 import tsplib95
@@ -11,18 +13,20 @@ def read_data_txt(filename):
 
     for line in range(1, len(file_lines)):
         list_of_strings = file_lines[line].split()
-        adjacency_matrix.append(list(map(int, list_of_strings)))
+        adjacency_matrix.append(list(map(float, list_of_strings)))
 
     adjacency_matrix = np.array(adjacency_matrix)
-    return adjacency_matrix, vertex_number
+    graph = networkx.from_numpy_matrix(adjacency_matrix)
+    adjacency_matrix = insert_inf(adjacency_matrix)
+    return adjacency_matrix, vertex_number, graph
 
 
 def read_data_tsp(filename):
     tsp = tsplib95.load(filename)
     graph = tsp.get_graph()
     adjacency_matrix = networkx.to_numpy_array(graph)
-
-    return adjacency_matrix, len(adjacency_matrix)
+    adjacency_matrix = insert_inf(adjacency_matrix)
+    return adjacency_matrix, len(adjacency_matrix), graph
 
 def read_ini():
     parameters = []
@@ -34,3 +38,10 @@ def read_ini():
         parameters.append(instance_parameters)
 
     return parameters
+
+def insert_inf(matrix):
+    length = len(matrix)
+    for index, row in enumerate(matrix):
+        row[index] = math.inf
+
+    return matrix
